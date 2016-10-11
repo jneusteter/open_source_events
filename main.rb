c5d4a6f2ad6c5ee39f66ec3730f3b92d60b011fa
John Neusteter <jneusteter@gmail.com>
2016-10-11 18:45:05 -0400

    Uncommented main code. Removed test code.

--- main.rb
+++ main.rb
@@ -5,13 +5,9 @@ require 'yaml'
 
 require './keys.rb'
 
-# @events = []
-# $client.mentions_timeline.each do |tweet|
-#   @events << tweet.text
-# end
-#
-# File.write('events.yml', @events.to_yaml)
-
-YAML.load_file('events.yml').each do |t|
-  puts t.gsub('@jneusteter', '')
+@events = []
+$client.mentions_timeline.each do |tweet|
+  @events << tweet.text
 end
+
+File.write('events.yml', @events.to_yaml)
