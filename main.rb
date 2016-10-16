be05f1070b6c309c3d4848c4a19d7ec21f070362
John Neusteter <jneusteter@gmail.com>
2016-10-16 09:22:45 -0400

    Create a command parser

--- main.rb
+++ main.rb
@@ -11,3 +11,44 @@ $client.mentions_timeline.each do |tweet|
 end
 
 File.write('events.yml', @events.to_yaml)
+
+class Event
+  def initialize(command)
+    @command = command
+  end
+
+  def array
+    @command.gsub(' -', '-').split('-')
+  end
+
+  def add?
+    array.include?('add')
+  end
+
+  def name
+    name = array.select { |name| name[0..3] == 'name' }[0]
+    if name.nil?
+      # nothing
+    else
+      name.gsub('name ', '')
+    end
+  end
+
+  def starts
+    starts = array.select { |starts| starts[0..5] == 'starts' }[0]
+    if starts.nil?
+      # nothing
+    else
+      Date.parse(starts.gsub('starts ', ''))
+    end
+  end
+
+  def ends
+    ends = array.select { |ends| ends[0..3] == 'ends' }[0]
+    if ends.nil?
+      # nothing
+    else
+      Date.parse(ends.gsub('ends', ''))
+    end
+  end
+end
